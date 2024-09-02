2cda06aefa04980c539d6100b47869e673bc5f4f
Uththara Samadhi <uththarasamadhi66@gmail.com>
2024-09-02 13:05:35 +0530

    Initial commit

--- src/test/java/ReviseTest.java
+++ src/test/java/ReviseTest.java
@@ -1,13 +1,12 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class ReviseTest {
+public class ReviseTest{
 
-    public static void main(String[] args) {
+    public static void main(String[]args){
 
-        WebDriver driver=new ChromeDriver();
-        driver.get("https://www.google.com/");
-
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://odel.lk/home");
         driver.quit();
     }
 }
